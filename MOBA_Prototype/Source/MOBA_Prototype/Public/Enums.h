99eb118fbb1d387cf47891fe2bb6ce015e36f392
Paul Seren-Rosso <p.serenrosso@rubika-edu.com>
2024-03-27 11:39:58 +0100

    add game instance and helpers

---
+++ MOBA_Prototype/Source/MOBA_Prototype/Public/Enums.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Enums.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class ETeam : uint8
+{
+	Neutral, Team1, Team2
+};
+
+UCLASS()
+class MOBA_PROTOTYPE_API UEnums : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+
+};
